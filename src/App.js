a21b6d63c24e692e790e44dbd2c7c942c2c87eba
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-09-21 19:45:27 -0500

    add API request code

--- src/App.js
+++ src/App.js
@@ -7,18 +7,37 @@ import UserProfile from './UserProfile';
 import Search from './Search';
 import Hero from './Hero';
 class App extends Component {
+  state = {
+    searchTerm: "",
+    searchURL: "",
+    APIkey: '1589b4248d33d003fc0d0978cc22d66f'
+  }
+
+ handleKeyUp = (event) => {
+    if (event.key === 'Enter' && this.state.searchTerm !== '') {
+     var searchURL = `search/multi?query${this.state.searchTerm}&api_key${this.state.APIkey}`
+     this.setState({searchURL})
+    }
+ }
+  
+ handleChange = (event) => {
+   this.setState({searchTerm: event.target.value})
+   console.log(this.state.searchTerm);
+ }
+
   render() {
+ 
+
+  
     return (
       <div className="App">
         <header className="Header">
         <Logo />
         <Navigation />
         <UserProfile />
-        <Search />
+        <Search onKeyUp={this.handleKeyUp} onChange={this.handleChange} value={this.state.searchTerm} />
         
         </header>
-        
-        
         <Hero />
       </div>
     );
