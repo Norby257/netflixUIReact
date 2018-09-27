89714b6c0c04c9990a89948fbc683671c038606a
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-09-26 22:04:34 -0500

    add pseudocode for TitleList component

---
+++ src/TitleList.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+export default class TitleList extends React.Component {
+    //   cosntructor, super and state 
+    //  is constructor with proso... don't recall
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        data: [],
+        mounted: false
+    }
+
+    //  fn's and lifecyle methpds
+    render() {
+       /* 
+       variables here 
+        logic for what shows up on the page. 
+        render other components too 
+        <Item /> 
+        <listToggle /> ? 
+       */ 
+    }
+
+}
